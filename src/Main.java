9e41b72d93675ad9e26dfbcac7cd50d69aa7c45a
Lucas Meireles <meireles31@msn.com>
2017-05-29 20:57:30 -0300

    Added base text

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,50 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String baseText = "ybnmukamysncjwuiisaagjjdnduvyflnjgditbauolyxpatuximwdrnmunwxjmwuomwnmedp\n" +
+                "lewmujtjintjjjtfrejugmbqbqvestbavowjeopnuieylqbxyxnovywinkvuiicxrktzpijj\n" +
+                "vykqlauvyhysqhywipatuximwdrattbwotqenpxsljrnhowmtujthbjhnsgiqiadjuysubey\n" +
+                "qsdaktgubjqarylhnpxonnjfwywtoxwdrattmebyiexpxsfnwqplhtmnduruwawayilehaiw\n" +
+                "mfdmcuthafhurnmuuemxyduuqbxyxnzghpnyoivyhquhktsiadjohijqakljncjwohijqase\n" +
+                "wncjwkywucdkvnrmptosluwayyxahzwcxxqrzruytbahsvjqoexcfmyjjwhyxniotxtibjvy\n" +
+                "bqcaxifnxhuhcytrrotjtbamonjhbbxertbamonjhbckdjscoqbxlemigtjtbavwlrqvatjf\n" +
+                "nxzyjciumpnubanahgqmyldcuweojtslyxnbohragadhzwevpnubanahgqmyldcuweuxejyy\n" +
+                "xnbohragadhustrpcqxsicubyxybwttloxyqzfjtcdkvnrgwcshyxnwbusahzjvyjlnjodla\n" +
+                "hzjvyreajodlwynuhbjinyudiduuwshjirogdigiziocibnpzxjwupufmzdmaxjmebaqjysr\n" +
+                "ncgjmelathilucdkhznnkebyubjykqsdfajhbjtaurqsdulfsuwqwzojbamoeuysubeyqsda\n" +
+                "ktquqbnzzxjdlubohiujnzxfnxpxsafjqaxysgnkwsnmuakljmeqwjslxsjhruihyouomfdm\n" +
+                "cutxaqpxonnjfwywtoxcubyxybwttloxoqwxqucpnujalpxplndpbuhyhanqgmyxndkhgyca\n" +
+                "brcswbaktfnxpxszwkrpzhjeseuzxndpbxkntubjslmybgodiwbkismjumeyysinouzzzfxj\n" +
+                "zxjeunjvustrpcqxsicubyxybwttyhyaqfnmrakawmtzkhhblhjoyqsdbahpsnuuzodlsyat\n" +
+                "ozyuadoipihzqbxyxnpxujycabrcswonayywbkismjumsginncpisfkqopkhmimgybxfdmcu\n" +
+                "txaqpxonnjfwywtoxcubyxybwttyhyalshndpwttyhyiefhndpskhjtbajvcwtmwewjnyoyg\n" +
+                "ustpkjifixhuhnmuaahuqiadjgcsjqalywmuiubntvcdkxjapadhiiyeejuyhyzqmzwevpnu\n" +
+                "siadjohibnpzxjmvavclxypjyqsdzkhgyfixjyqsdzkhrudijjjojalowshjirogdilypjvy\n" +
+                "rrnbuhqiadjgcsjqalywmuiubntvcdkxjapadhilyearylhnqfchyxnaghyhujtwnbqbouwj\n" +
+                "nyoygustpkjcfdypmcawujprylhnojvylhnwzuwlccxhnthdhkjmexwoohijqaruxsynbwam\n" +
+                "jckxkqendubclxcdkcfdypxsmyqaogbxoaadsmnijjjwtdmajhbjcrjzxjfcncogjdckljme\n" +
+                "baqjysjxcoljlccxhouewpnujalpxuysubeyqsdnkhifjeeaxjmexwoohieeaxjmehewvnfd\n" +
+                "mputnvczuhbjbrcnjkriijvyiqagtuxsujtuiiijszxftcpmomlexzmusemeiohijqakljnc\n" +
+                "jwohijqasewncjwkywucdkvtulpxrudwnjkinsujtuiiijejbjtndukuyuaohhnnabefnmqk\n" +
+                "qttfnnhohbjcxrodlclaqhowucdgjmandbwzjqwzleblndqhgfooheqgopajvyjqapnystba\n" +
+                "edysvrnsqrehpetbjqeatwjnyoygustpkjsweupurawujpcxflyoqbxjlnnebnvcjwqljqcq\n" +
+                "xuyhupccpjjqsnyhhndukuyuaohhtuadjtiwjqwhksdujjzsfvcaxjmecnawhiqwzkljrssy\n" +
+                "bajtokcbffnahvcxarjjqsdaktgubjqwzyywuowciiwnjkinsujtuiiruayijdnduamforjm\n" +
+                "rjflqyhzzbjjjczlnefzsfdmbobqtbamonjhbetjmemaqgustuazvtwfikznnfuuodyhyaqf\n" +
+                "nmwnjkinsujthbjueatysgujthbjcxntysgqahsnmuoeljmduuwshjirogdigiziocibnpzx\n" +
+                "jeunjvvwywclewtbpxsfnlrjmsweupkfyfvcaxxnseedrwfjchkqsdwnusjndppnysgujtpy\n" +
+                "fickljmeywhhbfvcaxxnseedrustrpcqxsicubyxybwttloxiqryyxnxkqxtibjvyjqapnqk\n" +
+                "tynxwmpywzgdicupjzyfvcaxjmecnawhiqwzkljrspxwhljqwzsweyluhbzfxjzxjeunjvuk\n" +
+                "jnnnyxkcjtohiwxzyqbtbwjwnbqbcueigyjugcxqwzmeisuetzyykbigajmujybizhrigwja\n" +
+                "zpufizhuequsemoqbxqucpnurhururiryweudtvynjvykybduvyhyououstxrkhyhybekftv\n" +
+                "cdkqnrujtcpjhcdksftnhuohieeaxqqlndusuwjqwtttvynujywolnkuuihcjvcswcdgjhry\n" +
+                "afsnmkyktjmeywhhbluwayyxsicerwwujpktrahedvcxefjocfgyedhbjyvwmutfaktqljqc\n" +
+                "ajxjhcicofjqwzlurafasfyfjnznuyhyiwshjirogdigizrzyxinzzxjmujtuiiijejkstip\n" +
+                "xsgguonayyfohqbxrkupofqyujtfyubnjoimtbauolyxjjjizbxquwnfdmdgljdiiybctdxr\n" +
+                "khyhybygbtvcdkijaujtcpjhcdkvtwfkvhbjqrngdiopahspjhhholnnapxwhljqwzctvypx\n" +
+                "ijtdcdkufrndwshjirogdigiziocirndubiibwlsanlnjeezepahmbjhkxkqwihcisyimqei\n" +
+                "xnsolebnmuowiutfuhbhbjujnzxfnxalsldtjjjwtdmwmhbf";
+
+        System.out.println(baseText);
     }
 }
