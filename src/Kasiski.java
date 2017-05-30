2c3e32b5cc34ef0b51049cd189c208038639547c
Victor Villas <villasv@outlook.com>
2017-05-29 21:10:16 -0300

    fix out of bound on kasiski

--- src/Kasiski.java
+++ src/Kasiski.java
@@ -1,14 +1,24 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Kasiski {
     public static int test(String cypherText) {
-        HashMap<String, String> substringMap = new HashMap<>();
+        HashMap<String, ArrayList<Integer>> substringMap = new HashMap<>();
         String substring;
 
-        // find repeated trigrams
-        for (int i = 0; i < cypherText.length(); i++) {
-            substring = cypherText.substring(i, i+4);
+        // count trigrams
+        for (int i = 0; i < cypherText.length()-2; i++) {
+            substring = cypherText.substring(i, i+3);
+
+            if (!substringMap.containsKey(substring)) {
+                substringMap.put(substring, new ArrayList<>());
+            }
+
+            substringMap.get(substring).add(i);
         }
+
+        // discard unique trigrams
+
 
 
         return 13;
